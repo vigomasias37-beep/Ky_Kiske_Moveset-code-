489c0b8ba1579c85ed871d70a802d3572b41a2c8
vigomasias37-beep <vigomasias37@gmail.com>
2026-08-26 13:50:38 -0500

    Add files via upload

---
+++ src/ken/acmd/normals/attackdash.rs
@@ -0,0 +1,36 @@
+use {
+    smash::{
+        lua2cpp::*,
+        phx::*,
+        app::{sv_animcmd::*, lua_bind::*},
+        lib::lua_const::*
+    },
+    smashline::*,
+    smash_script::*
+};
+
+unsafe extern "C" fn game_attackdash(agent: &mut L2CAgentBase) {
+    if macros::is_excute(agent) {
+        WorkModule::on_flag(agent.module_accessor, *FIGHTER_RYU_INSTANCE_WORK_ID_FLAG_FINAL_HIT_CANCEL);
+    }
+    frame(agent.lua_state_agent, 7.0);
+    if macros::is_excute(agent) {
+        macros::ATTACK(agent, 0, 0, Hash40::new("top"), 12.0, 50, 65, 0, 80, 4.2, 0.0, 9.0, 10.0, Some(0.0), Some(9.0), Some(7.2), 1.5, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_F, false, 2, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_normal"), *ATTACK_SOUND_LEVEL_L, *COLLISION_SOUND_ATTR_KEN_KICK, *ATTACK_REGION_KICK);
+    }
+    frame(agent.lua_state_agent, 10.0);
+    if macros::is_excute(agent) {
+        AttackModule::clear(agent.module_accessor, 0, false);
+        macros::ATTACK(agent, 0, 0, Hash40::new("top"), 8.0, 80, 60, 0, 80, 3.5, 0.0, 9.0, 20.0, Some(0.0), Some(9.0), Some(7.2), 1.5, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_F, false, 2, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_normal"), *ATTACK_SOUND_LEVEL_M, *COLLISION_SOUND_ATTR_KEN_KICK, *ATTACK_REGION_KICK);
+    }
+    frame(agent.lua_state_agent, 16.0);
+    if macros::is_excute(agent) {
+        WorkModule::off_flag(agent.module_accessor, *FIGHTER_RYU_INSTANCE_WORK_ID_FLAG_FINAL_HIT_CANCEL);
+    }
+    if macros::is_excute(agent) {
+        AttackModule::clear(agent.module_accessor, 0, false);
+    }
+}
+
+pub fn install(agent: &mut smashline::Agent) {
+    agent.acmd("game_attackdash", game_attackdash, smashline::Priority::Default);
+}
---
+++ src/ken/acmd/specials/mod.rs
@@ -0,0 +1,5 @@
+mod specialn;
+
+pub fn install(agent: &mut smashline::Agent) {
+    specialn::install(agent);
+}
